e22161db2a27965a2983d98d3d53071088e93acd
Markov41 <analogy48@yahoo.com>
2017-11-24 14:31:16 -0600

    Add computer AI

--- scripts/pong.js
+++ scripts/pong.js
@@ -170,3 +170,25 @@ Ball.prototype.update = function(paddle1, paddle2) {
         }
     }
 };
+
+var update = function() {
+    player.update();
+    computer.update(ball);
+    ball.update(player.paddle, computer.paddle);
+};
+
+Computer.prototype.update = function(ball) {
+    var x_pos = ball.x;
+    var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
+    if (diff < 0 && diff < -4) { // max speed left
+        diff = -5;
+    } else if (diff > 0 && diff > 4) { // max speed right
+        diff = 5;
+    }
+    this.paddle.move(diff, 0);
+    if (this.paddle.x < 0) {
+        this.paddle.x = 0;
+    } else if (this.paddle.x + this.paddle.width > 400) {
+        this.paddle.x = 400 - this.paddle.width;
+    }
+};
