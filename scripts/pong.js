8b4b2645779b79a69c8aea7b637c807ecc711f96
Markov41 <analogy48@yahoo.com>
2017-11-24 14:45:57 -0600

    Add AI player

--- scripts/pong.js
+++ scripts/pong.js
@@ -23,8 +23,9 @@ var step = function() {
 };
 
 var update = function() {
-    ball.update(player.paddle, computer.paddle);
     player.update();
+    computer.update(ball);
+    ball.update(player.paddle, computer.paddle);
 };
 
 var render = function() {
@@ -168,5 +169,21 @@ Ball.prototype.update = function(paddle1, paddle2) {
             this.x_speed += (paddle2.x_speed / 2);
             this.y += this.y_speed;
         }
+    }
+};
+
+Computer.prototype.update = function(ball) {
+    var x_pos = ball.x;
+    var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
+    if (diff < 0 && diff < -4) { // max speed left
+        diff = -5;
+    } else if (diff > 0 && diff > 4) { // max speed right
+        diff = 5;
+    }
+    this.paddle.move(diff, 0);
+    if (this.paddle.x < 0) {
+        this.paddle.x = 0;
+    } else if (this.paddle.x + this.paddle.width > 400) {
+        this.paddle.x = 400 - this.paddle.width;
     }
 };
